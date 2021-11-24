1dba5ee7f91debeb52a3b88ab994c47249e04333
Connor <conmmcguire@gmail.com>
2021-11-23 19:19:17 -0600

    add mapbox and hide api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map" style='width: 600px; height: 400px'></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 export default {
+  data: function () {
+    return {
+      map: [],
+    };
+  },
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted: function () {
+    this.generateMap();
+  },
+  methods: {
+    generateMap: function() {
+      mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_KEY;
+      const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+      })
+    }
   }
 }
 </script>
